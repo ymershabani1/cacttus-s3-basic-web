3dea4bdec845948f6d8de7952cd484d9c506ac0e
ymershabani1 <ymershabani@gmail.com>
2021-01-15 19:54:20 +0100

    test
    test

--- cacttus-s3-basic-web-master/task-management-tool/index.php
+++ cacttus-s3-basic-web-master/task-management-tool/index.php
@@ -10,6 +10,14 @@
 </head>
 <body>
 <center>
-</center>
+        <img width="400" src="https://cacttus.education/wp-content/uploads/2019/07/fb_CACTTUS_logo.png"> </img>
+        <form method="POST" action="/task-management-tool/login_logic.php">
+            <label>E-mail:</label><br>
+            <input type="email" name="email" /><br>
+            <label>Password:</label><br>
+            <input type="password" name="password" /><br><br>
+            <input type="submit" value="Log In" />
+        </form>
+    </center>
 </body>
 </html>
