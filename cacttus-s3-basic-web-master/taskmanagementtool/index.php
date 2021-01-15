4d810a523dc0628d04d53c6c5bbf6159152e2d4f
ymershabani1 <ymershabani@gmail.com>
2021-01-15 20:37:07 +0100

    tt
    ttt

--- cacttus-s3-basic-web-master/taskmanagementtool/index.php
+++ cacttus-s3-basic-web-master/taskmanagementtool/index.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>task management toll</title>
 </head>
 <body>
     <center>
@@ -13,6 +10,6 @@
         <label>Password:</label><br>
         <input type="password" name="password"/><br>
         <input type="submit" value="Login">
-</center>
+    </center>
 </body>
 </html>
