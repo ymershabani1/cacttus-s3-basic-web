db35e90a11bc7ee3d318790ab2870e052687b3d9
ymershabani1 <ymershabani@gmail.com>
2021-01-15 20:31:00 +0100

    tt
    tt

---
+++ cacttus-s3-basic-web-master/taskmanagementtool/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <form method="POST" action=""></form>
+        <label>Email:</label><br>
+        <input type="email" name="email"/><br>
+        <label>Password:</label><br>
+        <input type="password" name="password"/><br>
+        <input type="submit" value="Login">
+</center>
+</body>
+</html>
